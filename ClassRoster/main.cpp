fc7024cd866da0001c1686ff4ef37f50428ae1c8
David Praseuth <davidrpraseuth@gmail.com>
2023-02-22 12:22:35 -0800

    Initial Commit

---
+++ ClassRoster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ClassRoster
+//
+//  Created by David Praseuth on 2/22/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
